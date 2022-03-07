13be3bb34da40fd194f5826e724b90e7307cba55
Philip Mahama Akpanyi <mahamaphilip@yahoo.com>
2022-03-06 20:29:56 -0400

    Update EventListener.js

--- src/components/EventListener.js
+++ src/components/EventListener.js
@@ -4,7 +4,7 @@ import CircularProgressBar from "./CircularProgressBar";
 
 export default function EventListener({ showMeter }) {
   const [key, setKey] = useState("e");
-  let percentage = (920 / 5000) * 100;
+  let percentage = (900 / 5000) * 100;
 
   useEventListener("keydown", e => {
     if (e.key.toLowerCase() === "m" || e.key.toLowerCase() === "e")
