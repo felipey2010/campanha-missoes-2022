295cb0ccab6477afd19cc44139ca520133d69edc
felipey2010 <mahamaphilip@yahoo.com>
2022-03-12 20:06:33 -0400

    alvometro atualizado em 12/04

--- src/components/EventListener.js
+++ src/components/EventListener.js
@@ -4,11 +4,7 @@ import CircularProgressBar from "./CircularProgressBar";
 
 export default function EventListener({ showMeter }) {
   const [key, setKey] = useState("e");
-<<<<<<< HEAD
   let percentage = (1115 / 5000) * 100;
-=======
-  let percentage = (900 / 5000) * 100;
->>>>>>> 13be3bb34da40fd194f5826e724b90e7307cba55
 
   useEventListener("keydown", e => {
     if (e.key.toLowerCase() === "m" || e.key.toLowerCase() === "e")
